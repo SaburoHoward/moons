37a4f098ece88fb43aa164e87d00261e6dfcc92f
Saburo Howard <saburo.howard@uzh.ch>
2026-02-13 09:27:47 +0100

    Renamed run_moons into run_jupiter

---
+++ run_jupiter.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from moons import SatelliteModel
+import time
+
+#Merci de commencer par les couches les plus INTERNES. Rayon en cm et densité en g/cc.
+#Veillez à ce que la masse de la dernière couche soit similaire à la masse totale de la planète...
+
+#Jupiter w/ a polytrope
+layers_jupiter = [
+#    {"name": "Noyau", "mass": 1.898e28, "eos": "polytrope", "n": 1., "K": 2.003565e12},
+#    {"name": "Manteau", "mass": 1.898e29, "eos": "polytrope", "n": 1, "K": 2.003565e12},
+    {"name": "Envelope", "mass": 1.8982532e30, "eos": "polytrope", "n": 1., "K": 2.003565e12},
+]
+
+#Jupiter as a pure H-He ball (CMS19 EOS)
+Mplanet = 1.8982532e30
+"""
+layers_jupiter = [
+ #   {"name": "Noyau", "mass": Mplanet/100, "eos": "mixture","nbelem":2,"files":["Chabrier2019-H.csv","Chabrier2019-He.csv"],"mass_fractions":[0.73,0.27],"T_struct":"isentrope"},
+ #   {"name": "Manteau", "mass": Mplanet/10, "eos": "mixture","nbelem":2,"files":["Chabrier2019-H.csv","Chabrier2019-He.csv"],"mass_fractions":[0.73,0.27],"T_struct":"isentrope"},
+    {"name": "Envelope", "mass": Mplanet, "eos": "mixture","nbelem":2,"files":["Chabrier2019-H.csv","Chabrier2019-He.csv"],"mass_fractions":[0.73,0.27],"T_struct":"isentrope"},
+]
+"""
+layers_jupiter = [
+    {"name": "Manteau", "mass": Mplanet*0.04, "eos": "hm1989_rocks","roches": True,"T_struct":"isotherm"},
+    {"name": "Envelope", "mass": Mplanet, "eos": "mixture","nbelem":2,"files":["Chabrier2019-H.csv","Chabrier2019-He.csv"],"mass_fractions":[0.73,0.27],"T_struct":"isentrope"},
+]
+
+t0 = time.time()
+jupiter = SatelliteModel("Jupiter", Mplanet, layers_jupiter, nlayers=4000, distribution_type='erf')
+jupiter.integrate_structure_iterate(max_iter=100,rtol=1e-5,debug=False,P_surf=1e6,T_surf=165.,from_scratch=True,save=False)
+R_cm, MoI = jupiter.moment_of_inertia()
+print(R_cm/1e5)
+#print(MoI/(Mplanet*(R_cm)**2))
+J = jupiter.get_gravity()
+print(J)
+t1 = time.time()
+print("Temps écoulé :", t1 - t0, "s")
+#jupiter.plot()
